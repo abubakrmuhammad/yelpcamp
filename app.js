b2a42655a72e562536fc8895ca736fd1fdf57675
Abubakr <realchampiontheno1@gmail.com>
2019-07-10 13:44:52 +0500

    env PORT fix

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
 // Listen on port 3000
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('YelpCamp server has started!');
 });
